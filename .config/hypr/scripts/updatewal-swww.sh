5d8aa9c54834355bb3a28e343a99d35cb7419bd9
John S. Barden <john.barden@outlook.com>
2026-03-08 18:26:17 -0500

    some waybar configs

--- .config/hypr/scripts/updatewal-swww.sh
+++ .config/hypr/scripts/updatewal-swww.sh
@@ -9,6 +9,7 @@ newwall=$(echo $wallpaper | sed "s|$HOME/Wallpapers/||g")
 
 swww img $wallpaper --transition-step 20 --transition-fps=20
 ~/.config/scrIpts/waybar-launch.sh
+pkill -USR1 ghostty 2>/dev/null || true
 notify-send "Theme and Wallpaper updated" "With image $newwall"
 
 echo "DONE!"
