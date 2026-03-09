8c3809187c243bab09cda21fc8d9fddca78d398b
John S. Barden <john.barden@outlook.com>
2026-03-08 19:59:44 -0500

    chore: remove duplicate root hypr/ (keeping .config/hypr/)

--- hypr/scripts/spootify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PULSE_PROP="module-stream-restore.id=spotify"
-exec spotblock-rs & disown
-exec /usr/bin/spotify "$@"
--- hypr/scripts/waybar-launch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-killall waybar && waybar & disown
-# pkill -RTMIN+1 waybar
